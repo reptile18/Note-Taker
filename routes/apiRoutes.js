dbcf23ca8b9b3bd62e46f6245b67137d7a2c3741
Johnny Li <reptile18@gmail.com>
2020-06-12 09:40:21 -0700

    api routes working

---
+++ routes/apiRoutes.js
@@ -0,0 +1,61 @@
+const fs = require("fs");
+
+
+module.exports = app => {
+  app.get("/api/notes", (req,res) => {
+    fs.readFile("./db/db.json", (err, data) => {
+      if (err) {
+        res.status(503);
+        res.end();
+      }
+      else {
+        res.json(JSON.parse(data));
+      }
+    });
+  });
+
+  app.post("/api/notes", (req,res) => {
+    fs.readFile("./db/db.json", (err, data) => {
+      if (err) {
+        res.status(503);
+        res.end();
+      }
+      else {
+        const notesArray = JSON.parse(data);
+        const id = notesArray.length;
+        const note = {...req.body,"id": id};
+        notesArray.push(note);
+        fs.writeFile("./db/db.json", JSON.stringify(notesArray), err => {
+          if (err) {
+            res.status(503);
+            res.end();
+          }
+          else {
+            res.json(note);
+          }
+        });
+      }
+    });
+  });
+
+  app.delete("/api/notes/:id", (req,res) => {
+    var selectedNoteID = parseInt(req.params.id);
+    console.log(`id is ${selectedNoteID}, type is ${typeof selectedNoteID}`);
+    fs.readFile("./db/db.json", (err, data) => {
+      if (err) {
+        res.status(503);
+        res.end();
+      }
+      else {
+        let notesArray = JSON.parse(data);
+        notesArray = notesArray.filter(note => note.id !== selectedNoteID);
+        fs.writeFile("./db/db.json", JSON.stringify(notesArray), err => {
+          if (err) {
+            res.status(503);
+            res.end();
+          }
+        });
+      }
+    });
+  });
+}
